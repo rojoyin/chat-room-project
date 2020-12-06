146358e9cd968013f973fe82387005cf42d66e6a
Juan Jimenez <rojoyin@gmail.com>
2020-12-05 23:50:08 -0500

    Support for message creation

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class ChatRoom(models.Model):
     name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    content = models.CharField(max_length=512)
+    created_on = models.DateTimeField(auto_now_add=True, editable=False)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,8 +4,16 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
-def create_user(**params):
-    return get_user_model().objects.create_user(**params)
+user_payload = {'username': 'test', 'password': 'test123'}
+chat_room_payload = {'name': 'testing'}
+
+
+def create_user():
+    return get_user_model().objects.create_user(**user_payload)
+
+
+def create_chat_room():
+    return models.ChatRoom.objects.create(**chat_room_payload)
 
 
 class ModelTests(TestCase):
@@ -14,3 +22,14 @@ class ModelTests(TestCase):
             name='default'
         )
         self.assertEqual(str(room), room.name)
+
+    def test_create_message_ok(self):
+        user = create_user()
+        room = create_chat_room()
+        sample_content = 'hello'
+        message = models.Message.objects.create(
+            user=user, room=room, content=sample_content
+        )
+        self.assertEqual(message.user.username, user_payload['username'])
+        self.assertEqual(message.room.name, chat_room_payload['name'])
+        self.assertEqual(message.content, sample_content)
