4e858bf2896eb3218533b33e48c2d6d63e58da5d
Juan Jimenez <rojoyin@gmail.com>
2020-12-06 19:27:24 -0500

    Add login and signup base

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+from user.views import login, signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
     path('api/chat/', include('chat.urls')),
-    path('auth/', obtain_auth_token)
+    path('auth/', obtain_auth_token),
+    path('login/', login, name='login'),
+    path('signup/', signup, name='signup'),
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,16 @@
 from rest_framework import generics
+from django.shortcuts import render
 
 from .serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+def login(request):
+    return render(request, "login.html")
+
+
+def signup(request):
+    return render(request, "signup.html")
