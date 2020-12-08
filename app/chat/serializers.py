09fab5983e5c7f6fba2a6fed91ec12d8e183a448
Juan Jimenez <rojoyin@gmail.com>
2020-12-07 20:44:57 -0500

    Get username to render message

--- app/chat/serializers.py
+++ app/chat/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from core.models import ChatRoom, Message
+from user.serializers import UserSerializer
 
 
 class ChatRoomSerializer(serializers.ModelSerializer):
@@ -11,6 +12,8 @@ class ChatRoomSerializer(serializers.ModelSerializer):
 
 
 class MessageSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=False)
+
     class Meta:
         model = Message
         fields = ('id', 'user', 'content', 'created_on')
